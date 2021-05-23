00a5d168d7c5da2ba4881f2b892a1d8cb9e9f46d
Aaditya Menon <aaditya@sparker0i.me>
2021-05-23 15:56:13 +0530

    Reworked with PassportJS

--- index.js
+++ index.js
@@ -1,100 +1,72 @@
-const express = require('express')
-const session = require('express-session')
-const cookieParser = require('cookie-parser')
+var express = require('express');
+var passport = require('passport');
+var Strategy = require('passport-twitter').Strategy;
 
-const {
-  getOAuthRequestToken,
-  getOAuthAccessTokenWith,
-  oauthGetUserById,
-  postTweet
-} = require('./oauth-utilities')
-
-const path = require('path')
-const fs = require('fs')
-const { resolveSoa } = require('dns')
-
-const TEMPLATE = fs.readFileSync(path.resolve(__dirname, 'client', 'template.html'), { encoding: 'utf8' })
-
-const COOKIE_SECRET = process.env.npm_config_cookie_secret || process.env.COOKIE_SECRET
-
-main()
-  .catch(err => console.error(err.message, err))
-
-async function main () {
-  const app = express()
-  app.use(cookieParser())
-  app.use(session({ secret: COOKIE_SECRET || 'secret' }))
-
-  app.listen(3000, () => console.log('listening on http://127.0.0.1:3000'))
-
-  app.get('/', async (req, res, next) => {
-    console.log('/ req.cookies', req.cookies)
-    if (req.cookies && req.cookies.twitter_screen_name) {
-      console.log('/ authorized', req.cookies.twitter_screen_name)
-      return res.send(TEMPLATE.replace('CONTENT', `
-        <h1>Hello ${req.cookies.twitter_screen_name}</h1>
-        <br>
-        <a href="/twitter/search">Search for Some tweets</a>
-        <br>
-        <a href="/twitter/logout">logout</a>
-      `))
-    }
-    return next()
-  })
-
-  app.use(express.static(path.resolve(__dirname, 'client')))
-
-  app.get('/twitter/logout', logout)
-  function logout (req, res, next) {
-    res.clearCookie('twitter_screen_name')
-    req.session.destroy(() => res.redirect('/'))
-  }
-
-  app.get('/twitter/search', async(req, res, next) => {
-    var authToken =  req.session.oauthAccessToken
-    var authSecret = req.session.oauthAccessTokenSecret
-    const tweetDetails = await postTweet({ authToken, authSecret })
-    res.send(tweetDetails)
-  })
-
-  app.get('/twitter/authorize', twitter('authorize'))
-  function twitter (method = 'authorize') {
-    return async (req, res) => {
-      const { oauthRequestToken, oauthRequestTokenSecret } = await getOAuthRequestToken()
-      console.log(`/twitter/${method} ->`, { oauthRequestToken, oauthRequestTokenSecret })
-
-      req.session = req.session || {}
-      req.session.oauthRequestToken = oauthRequestToken
-      req.session.oauthRequestTokenSecret = oauthRequestTokenSecret
-
-      const authorizationUrl = `https://api.twitter.com/oauth/${method}?oauth_token=${oauthRequestToken}`
-      console.log('redirecting user to ', authorizationUrl)
-      res.redirect(authorizationUrl)
-    }
-  }
-
-  app.get('/twitter/callback', async (req, res) => {
-    const { oauthRequestToken, oauthRequestTokenSecret } = req.session
-    const { oauth_verifier: oauthVerifier } = req.query
-    console.log('/twitter/callback', { oauthRequestToken, oauthRequestTokenSecret, oauthVerifier })
-
-    const { oauthAccessToken, oauthAccessTokenSecret, results } = await getOAuthAccessTokenWith({ oauthRequestToken, oauthRequestTokenSecret, oauthVerifier })
-    console.log('Access Token', oauthAccessToken)
-    console.log('Access Secret', oauthAccessTokenSecret)
-    console.log('Results', results)
-
-    const { user_id: userId /*, screen_name */ } = results
-    const user = await oauthGetUserById(userId, { oauthAccessToken, oauthAccessTokenSecret })
-
-    req.session.twitter_screen_name = user.screen_name
-    res.cookie('twitter_screen_name', user.screen_name, { maxAge: 900000, httpOnly: true })
-    req.session.oauthAccessToken = oauthAccessToken
-    req.session.oauthAccessTokenSecret = oauthAccessTokenSecret
-
-    res.cookie('oauthAccessToken', oauthAccessToken)
-    res.cookie('oauthAccessTokenSecret', oauthAccessTokenSecret)
-
-    console.log('user succesfully logged in with twitter', user.screen_name)
-    req.session.save(() => res.redirect('/'))
-  })
+var trustProxy = false;
+if (process.env.DYNO) {
+  trustProxy = true;
 }
+
+passport.use(new Strategy({
+    consumerKey: process.env['TWITTER_CONSUMER_API_KEY'],
+    consumerSecret: process.env['TWITTER_CONSUMER_API_SECRET_KEY'],
+    callbackURL: '/twitter/callback',
+    proxy: trustProxy
+  },
+  function(token, tokenSecret, profile, cb) {
+    return cb(null, profile);
+  }));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(require('morgan')('combined'));
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/',
+  function(req, res) {
+    res.render('home', { user: req.user });
+  });
+
+app.get('/login',
+  function(req, res){
+    res.redirect('/login/twitter')
+  });
+
+app.get('/login/twitter',
+  passport.authenticate('twitter'));
+
+app.get('/twitter/callback',
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    res.render('profile', { user: req.user });
+  });
+
+app.get('/logout',
+  function(req, res){
+    req.session.destroy(function (err) {
+      res.redirect('/');
+    });
+  });
+
+app.listen(3000, () => console.log('listening on http://127.0.0.1:3000'))
